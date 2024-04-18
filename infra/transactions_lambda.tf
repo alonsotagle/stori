25a6b45de5460965c819b2621553a8c15d4dff0d
Alonso Tagle <dev@alonsotagle.com>
2024-04-17 20:22:46 -0600

    feat: create transaction s3 bucket

---
+++ infra/transactions_lambda.tf
@@ -0,0 +1,17 @@
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "transactions"
+  description   = "Create an upload transactions"
+  handler       = "app.main"
+  runtime       = "python3.10"
+
+  source_path = "../transactions/src/transactions"
+
+  create_role = false
+  lambda_role = "arn:aws:iam::654654472464:role/tfc"
+
+  environment_variables = {
+    TRANSACTIONS_COUNT = "1000"
+  }
+}
