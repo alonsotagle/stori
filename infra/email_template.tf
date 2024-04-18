4b62067c37785d43023e67d21feb24d6ca0c2ffa
Alonso Tagle <dev@alonsotagle.com>
2024-04-18 04:04:30 -0600

    feat: add stori logo in email template

--- infra/email_template.tf
+++ infra/email_template.tf
@@ -1,5 +1,5 @@
 resource "aws_ses_template" "report_template" {
   name    = "report"
   subject = "Transactions Report"
-  html    = "<h1>Transactions Report</h1><p>Total balance is {{balance}}.</p><ul>{{#each transactions}}<li>Number of transactions in {{month}}: {{count}}</li>{{/each}}</ul><p>Average debit amount: {{debit}}</p><p>Average credit amount: {{credit}}</p>"
+  html    = "<h1>Transactions Report</h1><header><img src='https://es.wikipedia.org/wiki/Stori_%28Tarjeta_de_cr%C3%A9dito%29#/media/Archivo:Stori_Logo_2023.svg' alt="Stori"/></header><p>Total balance is {{balance}}.</p><ul>{{#each transactions}}<li>Number of transactions in {{month}}: {{count}}</li>{{/each}}</ul><p>Average debit amount: {{debit}}</p><p>Average credit amount: {{credit}}</p>"
 }
